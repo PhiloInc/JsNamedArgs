bb52a0f64079518e1fdb35e7d6d59aa5589757cf
Casey Morris <casey@philo.com>
2024-07-03 12:11:16 -0400

    first pass at CI to sonatype, v 0.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.vanniktech.maven.publish.MavenPublishBaseExtension
+import com.vanniktech.maven.publish.SonatypeHost
+
 buildscript {
     val kotlinVersion: String by project
     repositories {
@@ -10,15 +13,49 @@ buildscript {
 
 plugins {
     id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 allprojects {
     group = "com.philo"
-    version = "0.0.0"
+    version = "0.0.1"
 
     repositories {
         mavenLocal()
         mavenCentral()
         google()
+    }
+}
+
+
+subprojects {
+    plugins.withId("com.vanniktech.maven.publish.base") {
+        configure<MavenPublishBaseExtension> {
+            publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+            signAllPublications()
+            pom {
+                name.set(project.name)
+                description.set("Philo's KMP plugin to generate JS style functions and classes")
+                url.set("https://github.com/PhiloInc/JsNamedArgs")
+                inceptionYear.set("2024")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/PhiloInc/JsNamedArgs/blob/main/LICENSE.txt")
+                        distribution.set("repo")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/PhiloInc/JsNamedArgs.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:PhiloInc/JsNamedArgs.git")
+                    url.set("https://github.com/PhiloInc/JsNamedArgs")
+                }
+                developers {
+                    developer {
+                        name.set("Philo, Inc.")
+                    }
+                }
+            }
+        }
     }
 }
